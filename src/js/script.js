62f8a989e4d6153a1d4db1a4631424320e232a51
Viktor Plakyda <plakyda1@gmail.com>
2016-06-15 22:56:52 +0300

    gotovo

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,23 @@
-
-  $(function() {
+$(document).ready(function(){
+	$('.size-blocks__item').on('click', function(event) {
+		event.preventDefault();
+		if (!$(this).hasClass('active')) {
+			$('.size-blocks__item').each(function(index, el) {
+				$(this).removeClass('active')
+			});
+			$(this).addClass('active')
+		}
+	});
+// Спиннер
+	$(function() {
     $( "#spinner" ).spinner({
-      spin: function( event, ui ) {
-        if ( ui.value > 10 ) {
-          $( this ).spinner( "value", -10 );
-          return false;
-        } else if ( ui.value < -10 ) {
-          $( this ).spinner( "value", 10 );
-          return false;
-        }
-      }
+    	min: 0
     });
   });
-$(document).ready(function(){
-
+// Вкладки
+  $(function() {
+		$( "#tabs" ).tabs();
+	});
 	// Слайдер на главной странице
 	$('.carousel-main').owlCarousel({
   	items: 1,
