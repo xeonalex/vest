a30a26c6ec011ab09e06bffbd95f41ae9a09eac8
Viktor Plakyda <plakyda1@gmail.com>
2016-06-15 21:02:30 +0300

    add UI

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-console.log('hello from script.js');
+  $(function() {
+    $( "#spinner" ).spinner({
+      spin: function( event, ui ) {
+        if ( ui.value > 10 ) {
+          $( this ).spinner( "value", -10 );
+          return false;
+        } else if ( ui.value < -10 ) {
+          $( this ).spinner( "value", 10 );
+          return false;
+        }
+      }
+    });
+  });
