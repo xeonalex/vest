d2d3f3d1a8830a1d15b704be0dc342113174bfa2
Sasha Saulyak <lux1995@yandex.ru>
2016-06-15 17:54:14 +0300

    gulpfile changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,32 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),// Склейка файлов
     browserSync  = require('browser-sync'), // BrowserSync
-    
     jade = require('gulp-jade'), // Jade обработчик html
-    
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     cssnano = require('gulp-cssnano'), //Минификация CSS
     autoprefixer = require('gulp-autoprefixer'), // Автопрефиксы CSS
-    
     imagemin = require('gulp-imagemin'),// Сжатие JPG, PNG, SVG, GIF
-    
     uglify = require('gulp-uglify'), // Минификация JS
-
     plumber = require('gulp-plumber'),
-    watch = require('gulp-watch');
-
+    watch = require('gulp-watch'),
+    changed = require('gulp-changed');
 
 //Собираем Jade ( html )
+gulp.task('jade-includes', function() {
+  return gulp.src(['./src/jade/*.jade','!./src/jade/_*.jade'])
+    .pipe(plumber())
+    .pipe(jade({
+       pretty: true
+    }))
+    .on('error', console.log)
+    .pipe(gulp.dest('./build/'))
+    .pipe(browserSync.stream());
+  });
+//Собираем только изменившийся файл Jade ( html )
 gulp.task('jade-templates', function() {
   return gulp.src(['./src/jade/*.jade','!./src/jade/_*.jade'])
+    .pipe(changed('./build/', {extension: '.html'}))
     .pipe(plumber())
     .pipe(jade({
        pretty: true
@@ -34,7 +41,7 @@ gulp.task('sass-dev', function() {
   return gulp.src('src/sass/**/*.scss')
     .pipe(plumber())
     // .pipe(sourcemaps.init())
-    
+
     .pipe(sass({
       style: 'compressed',
       errLogToConsole: true,
@@ -53,7 +60,8 @@ gulp.task('sass-dev', function() {
 
 //Сжатие изображений
 gulp.task('img', function() {
-  return gulp.src('src/img/**/**/**')
+  return gulp.src(['src/img/**/**/*.png','src/img/**/**/*.jpg','src/img/**/**/*.svg'])
+    .pipe(changed('./build/img/'))
     .pipe(imagemin({ optimizationLevel: 3, progressive: true}))
     .pipe(gulp.dest('build/img/'));
 });
@@ -68,7 +76,6 @@ gulp.task('js', function(){
   .pipe(browserSync.stream());
 });
 
-
 //Копируем JS-vendor
 gulp.task('js-vendor', function(){
   return gulp.src('src/js/vendor/*.js')
@@ -79,10 +86,10 @@ gulp.task('js-vendor', function(){
   .pipe(browserSync.stream());
 });
 
-
 // Favicon
 gulp.task('favicon', function(){
   return gulp.src('src/favicon/*')
+  .pipe(changed('./build/favicon/'))
   .pipe(plumber())
   .pipe(gulp.dest('build/favicon/'))
   .pipe(browserSync.stream());
@@ -91,28 +98,30 @@ gulp.task('favicon', function(){
 // Fonts
 gulp.task('fonts', function(){
   return gulp.src('src/fonts/*')
+  .pipe(changed('./build/css/fonts/'))
   .pipe(plumber())
   .pipe(gulp.dest('build/css/fonts/'))
   .pipe(browserSync.stream());
 });
 
-
-
 // WATCH
-gulp.task('default', ['jade-templates','sass-dev','img','js-vendor','js','favicon','fonts'], function () {
-    
+gulp.task('default', ['jade-includes','sass-dev','img','js-vendor','js','favicon','fonts'], function () {
     browserSync.init({
-      server : './build'
+      server : './build',
+      host : '3002'
     });
 
-    watch('./src/jade/**/*.jade', function() {
+    watch('./src/jade/_includes/**/*.jade', function() {
+      gulp.start('jade-includes');
+    });
+    watch('./src/jade/*.jade', function() {
       gulp.start('jade-templates');
     });
 
     watch(["./src/sass/**/*.scss",'./src/sass/_*.scss'], function() {
       gulp.start('sass-dev');
     });
-    
+
     watch('./src/js/*.js', function() {
       gulp.start('js');
     });
@@ -121,7 +130,7 @@ gulp.task('default', ['jade-templates','sass-dev','img','js-vendor','js','favico
       gulp.start('js-vendor');
     });
 
-    watch('./src/img/**/*', function() {
+    watch(['./src/img/**/**/*.png','./src/img/**/**/*.jpg','./src/img/**/**/*.svg'], function() {
       gulp.start('img');
     });
 
